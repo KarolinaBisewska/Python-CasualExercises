5d325ffe92276e9d2035f461e97f85053f5e2932
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-11-01 22:59:26 +0100

    Update f_string_functions.py

--- f_string_functions.py
+++ f_string_functions.py
@@ -1,5 +1,11 @@
 # All f-string functions
 # The general shape is: f"{value:<alignment>signwidth,group.typeprecision}"
+# 1. Alignment (<  >  ^  =)
+# < Left-align (default for strings)
+# > Right-align (default for numbers)
+# ^ Center
+# = Force the padding to be placed between the sign and the digits (numbers only)
+
 
 
 value = 123
