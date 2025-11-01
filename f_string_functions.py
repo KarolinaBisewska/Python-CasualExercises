f4c463dd73068d994800940e402fc6d6608be896
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-11-01 23:05:32 +0100

    Update f_string_functions.py

--- f_string_functions.py
+++ f_string_functions.py
@@ -1,14 +1,15 @@
 # All f-string functions
 # The general shape is: f"{value:<alignment>signwidth,group.typeprecision}"
+
 # 1. Alignment (<  >  ^  =)
 # < Left-align (default for strings)
 # > Right-align (default for numbers)
 # ^ Center
 # = Force the padding to be placed between the sign and the digits (numbers only)
 
-print(f"{'abc':<10}+r")  output:  |abc       +r|
-print(f"{'abc':>10}+r")  output:  |       abc+r|
-print(f"{'abc':^10}+r")  output:  |   abc    +r|
-print(f"{-123:=10}+r")   output:  |-      123+r|
+print(f"{'abc':<10}+r")  # output:  |abc       +r|
+print(f"{'abc':>10}+r")  # output:  |       abc+r|
+print(f"{'abc':^10}+r")  # output:  |   abc    +r|
+print(f"{-123:=10}+r")   # output:  |-      123+r|
 
 
