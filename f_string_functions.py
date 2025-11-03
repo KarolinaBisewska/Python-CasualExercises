99cfaaa747236d2cf4e3b8fdab7bb8856a9c20ae
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-11-03 10:26:46 +0100

    Update f_string_functions.py

--- f_string_functions.py
+++ f_string_functions.py
@@ -1,11 +1,11 @@
-# All f-string functions
-# The general shape is: f"{value:<alignment>signwidth,group.typeprecision}"
-
-# 1. Alignment (<  >  ^  =)
-# < Left-align (default for strings)
-# > Right-align (default for numbers)
-# ^ Center
-# = Force the padding to be placed between the sign and the digits (numbers only)
+    # All f-string functions
+    # The general shape is: f"{value:<alignment>signwidth,group.typeprecision}"
+  
+    # 1. Alignment (<  >  ^  =)
+    # < Left-align (default for strings)
+    # > Right-align (default for numbers)
+    # ^ Center
+    # = Force the padding to be placed between the sign and the digits (numbers only)
 
 amount = 5
 print(f"{'abc':<10}+r")  # output:  |abc       +r|
@@ -13,4 +13,8 @@ print(f"{'abc':>10}+r")  # output:  |       abc+r|
 print(f"{'abc':^10}+r")  # output:  |   abc    +r|
 print(f"{-123:=10}+r")   # output:  |-      123+r|
 print(f"{amount:>3} {'max':>10}")   # output:  |  5        max|
+
+    # 2. Fill character (any char except { })
+
+
 
