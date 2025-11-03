b93a69f4b787ac8d220216f6222a2c646b5b0706
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-11-03 11:30:38 +0100

    Update f_string_functions.py

--- f_string_functions.py
+++ f_string_functions.py
@@ -22,6 +22,11 @@ print(f"{amount:*>10}")   # output:  |*********5|
 
 print(f"{'view'}{amount:10}")    # output:  |view         5|
 
+    # 4. Sign handling (+ - space)
+    #     + Always show sign (+ for positive, - for negative)
+    #     - Only show - for negative (default for numbers)
+
+
 
 
 
